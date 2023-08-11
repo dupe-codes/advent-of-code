50c2334c75c3327f892f9f46bc7ac40845a09cd2
dupe <njdupe@gmail.com>
2023-08-11 16:28:57 -0400

    feat: ✨ finish rock paper scissors

--- ocaml/bin/2022-rock-paper-scissors/main.ml
+++ ocaml/bin/2022-rock-paper-scissors/main.ml
@@ -1,4 +1,5 @@
 type move = Rock | Paper | Scissors
+type outcome = Win | Draw | Lose
 
 let opp_encoding_to_move move =
   match move with
@@ -9,13 +10,13 @@ let opp_encoding_to_move move =
 
 let player_encoding_to_move move =
   match move with
-  | "X" -> Rock
-  | "Y" -> Paper
-  | "Z" -> Scissors
+  | "X" -> Lose
+  | "Y" -> Draw
+  | "Z" -> Win
   | _ -> failwith "Invalid move"
 
-let get_move_score move =
-  match move with Rock -> 1 | Paper -> 2 | Scissors -> 3
+(*let get_move_score move =*)
+(*match move with Rock -> 1 | Paper -> 2 | Scissors -> 3*)
 
 let input = "./input"
 
@@ -24,25 +25,27 @@ let process_line line =
   let split_line = Str.split (Str.regexp " ") line in
   let opponent_move = opp_encoding_to_move (List.nth split_line 0) in
   let player_move = player_encoding_to_move (List.nth split_line 1) in
-  let move_score = get_move_score player_move in
   let result =
-    (match (opponent_move, player_move) with
-    | Rock, Paper -> 6
-    | Rock, Rock -> 3
-    | Paper, Scissors -> 6
-    | Paper, Paper -> 3
-    | Scissors, Rock -> 6
-    | Scissors, Scissors -> 3
-    | _, _ -> 0)
-    + move_score
+    match (opponent_move, player_move) with
+    | Rock, Win -> 8
+    | Rock, Lose -> 3
+    | Rock, Draw -> 4
+    | Paper, Win -> 9
+    | Paper, Lose -> 1
+    | Paper, Draw -> 5
+    | Scissors, Win -> 7
+    | Scissors, Lose -> 2
+    | Scissors, Draw -> 6
   in
   result
 
 let get_strategy_score input_file =
   let in_ch = open_in input_file in
-  let calculate_score () =
+  let rec calculate_score () =
     let line = try Some (input_line in_ch) with End_of_file -> None in
-    match line with None -> 0 | Some line -> process_line line
+    match line with
+    | None -> 0
+    | Some line -> process_line line + calculate_score ()
   in
   calculate_score ()
 
