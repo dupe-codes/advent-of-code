292bf647974366df8009bec3f071bffac80f3d54
dupe <njdupe@gmail.com>
2023-09-01 12:00:43 -0400

    feat: ✨ binary rust problem go away copilot you suck

---
+++ rust/src/binary.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+
+pub fn binary(input_file: &str) {
+    let commands = fs::read_to_string(input_file).unwrap();
+    let mut element_counts = [0; 12];
+    for line in commands.split("\n") {
+        if line != "" {
+            for (i, ch) in line.chars().enumerate() {
+                element_counts[i] += if ch == '0' { -1 } else { 1 }
+            }
+        }
+    };
+
+    println!("{:?}", element_counts);
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    for i in (0..12).rev() {
+        println!("{}", 1 << (11 - i));
+        if element_counts[i] > 0 {
+            gamma += 1 << (11 - i);
+        } else {
+            epsilon += 1 << (11 - i);
+        }
+    }
+    println!("gamma: {}, epsilon: {}", gamma, epsilon);
+    println!("result: {}", gamma*epsilon);
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,12 @@
 mod dive;
 mod sonar_sweep;
+mod binary;
 
 const DIVE_INPUT_FILE: &str = "inputs/dive.txt";
+const BINARY_INPUT: &str = "inputs/binary.txt";
 
 fn main() {
     //sonar_sweep::sonar_sweep();
-    dive::dive(DIVE_INPUT_FILE)
+    //dive::dive(DIVE_INPUT_FILE)
+    binary::binary(BINARY_INPUT);
 }
